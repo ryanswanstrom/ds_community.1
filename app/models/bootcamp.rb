c53811150dc36035c826ac41c704c98cfb08eb6e
Ryan <swGooF@users.noreply.github.com>
2014-09-17 21:30:35 +0000

    adding csv stuff

--- app/models/bootcamp.rb
+++ app/models/bootcamp.rb
@@ -1,4 +1,15 @@
 class Bootcamp < ActiveRecord::Base
   validates_formatting_of :url, using: :url
+  validates_presence_of :name, :url, :program
+  validates_uniqueness_of :url
   acts_as_votable 
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
