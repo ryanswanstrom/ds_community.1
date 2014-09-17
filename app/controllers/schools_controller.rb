404350b6ec6807cc38754d361fdbf16152bb7cf1
Ryan <swGooF@users.noreply.github.com>
2014-09-17 20:53:07 +0000

    adding new pages

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,4 +1,10 @@
 class SchoolsController < ApplicationController
   def colleges
+    @schools = School.all
+    respond_to do |format|
+      format.html
+      format.json { render text: @schools.to_json }
+      format.csv { render text: @schools.to_csv }
+    end
   end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,17 @@
 class School < ActiveRecord::Base
   has_many :school_extras
   validates_formatting_of :url, using: :url
+  validates_presence_of :name, :url, :degree, :program
+  validates_uniqueness_of :url
+  validates_inclusion_of  :degree, :in => %w( Masters Bachelors Doctorate Certificate )
   acts_as_votable 
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
