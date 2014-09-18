9285f94041d05b8a8c7aea1784a4fdda9e0ef864
Ryan <swGooF@users.noreply.github.com>
2014-09-18 23:55:41 +0000

    unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
+  validates_uniqueness_of :username
   attr_accessor :login
   
   #->Prelang (user_login:devise/username_login_support)
