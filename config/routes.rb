a8c364de8d94b3545fd54d7f46954cfe00c699ef
Ryan <ryanswanstrom@users.noreply.github.com>
2014-10-11 05:14:57 +0000

    login and logout and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'colleges', to: 'schools#colleges'
   get 'bootcamps', to: 'bootcamps#bootcamps'
+  get 'profile/:username', to: 'users#show'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -61,15 +62,15 @@ Rails.application.routes.draw do
   #   end
   
   #->Prelang (user_login:devise/stylized_paths)
-#  devise_scope :user do
-#    get    "login"   => "users/sessions#new",         as: :new_user_session
-#    post   "login"   => "users/sessions#create",      as: :user_session
-#    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
+  devise_scope :user do
+    get    "login"   => "users/sessions#new",         as: :new_user_session
+    post   "login"   => "users/sessions#create",      as: :user_session
+    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
     
-#    get    "signup"  => "users/registrations#new",    as: :new_user_registration
-#    post   "signup"  => "users/registrations#create", as: :user_registration
-#    put    "signup"  => "users/registrations#update", as: :update_user_registration
-#    get    "account" => "users/registrations#edit",   as: :edit_user_registration
-#  end
+    get    "signup"  => "users/registrations#new",    as: :new_user_registration
+    post   "signup"  => "users/registrations#create", as: :user_registration
+    put    "signup"  => "users/registrations#update", as: :update_user_registration
+    get    "account" => "users/registrations#edit",   as: :edit_user_registration
+  end
 
 end
